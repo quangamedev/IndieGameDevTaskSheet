002a5f4833a47e9fe1f3783afb1ff2a1adb82f96
Quan Nguyen <quan.0512@hotmail.com>
2020-10-03 20:06:20 +0700

    Created GamePIece class

---
+++ Indie_Match3/Assets/Scripts/GamePiece.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/***********************************
+Author: Quan Nguyen
+Date Made: 3.10.20
+Object(s) holding this script: GamePiece
+Summary:
+- 
+***********************************/
+
+public class GamePiece : MonoBehaviour
+{
+
+    public int xIndex; //store the current xPos of the game piece
+    public int yIndex; //store the current yPos of the game piece
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
